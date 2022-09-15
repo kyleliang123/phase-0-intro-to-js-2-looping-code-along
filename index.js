11a5b49496220c5187c67c1c4e747d7cf766da15
Kyle <kyleliang586@gmail.com>
2022-09-15 12:48:05 -0700

    commit tree 213490a5843238f1970557dea4eafd1ba1158b88

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const cards = (['Guadalupe', 'Ollie', 'Aki'])
+const event = "surprise"
+
+function writeCards(cards, event){
+    let firstName = []
+    for (let x = 0; x < cards.length; x++){
+        firstName.push(`Thank you, ${cards[x]}, for the wonderful ${event} gift!`)
+    }
+    return firstName
+}
+writeCards(cards)
+
+function countDown() {
+
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
